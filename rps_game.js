be3906baec70a46d6a682a89750d8efdc639c1dc
Tiffany Thompson <tdthompson899@gmail.com>
2016-08-12 15:56:37 -0500

    testing: displaying user input from form

--- rps_game.js
+++ rps_game.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   // Input time button is clicked:
-  $("#play_game").click(function(){
-    alert("Something");
+  $('.play_game').click(function(){
+    var min = $('#mins').val();
+    var sec = $('#secs').val();
+
+    $('.timer').html("Minutes: " + min + " Seconds: " + sec);
   });
 });
