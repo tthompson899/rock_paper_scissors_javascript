44dc3ce44ae6a9d19815dde55097a9509fb82213
Tiffany Thompson <tdthompson899@gmail.com>
2016-08-12 19:17:47 -0500

    Timer is working: timer starts where with the user input and stops at zero

--- rps_game.js
+++ rps_game.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
     var min = $('#mins').val();
     var sec = $('#secs').val();
 
-    $('.timer').html("Minutes: " + min + " Seconds: " + sec);
+    var clock = setInterval(getTime, 1000);
+
+    function getTime(){
+      sec -= 1; // decrease the seconds
+
+      if(sec <= 0){ // No more Seconds...
+        if (min >= 1) {// more mins so add 59 seconds
+          sec = 59;
+          min -= 1;
+        }
+        else{// no more time left
+          clearInterval(clock);
+        }
+      }
+      $('.timer').html("Minutes: " + min + " Seconds: " + sec);
+
+    }
   });
 });
